228e957e6ab566fb5af929528d0fc4b69826f05e
Kolia Parasochka <koliaparasochka99@gmail.com>
2023-06-27 15:52:25 +0300

    adds animation

---
+++ webclient/src/components/Services.js
@@ -0,0 +1,49 @@
+import { FaTruckMoving, FaTractor } from 'react-icons/fa';
+import { GiWheat } from 'react-icons/gi';
+import FadeInSection from './FadeInSection';
+
+function Services(props) {
+    const translate = props.content;
+
+    return (
+        <div id="services" className="services-section">
+            <div className="services-title">
+                {translate.servicesTitle}
+            </div>
+            <div className='services-list'>
+                <div className='service-block fade-right'>
+                        <FaTractor className='service-icon'/>
+                        <div className='service-label'>
+                            {translate.landCultivationTitle}
+                        </div>
+                        <hr className='horizontal-line'/>
+                        <div className='service-description'>
+                            {translate.landCultivationDescription}
+                        </div>
+                </div>
+                <div className='service-block fade-right'>
+                        <GiWheat className='service-icon'/>
+                        <div className='service-label'>
+                            {translate.agriculturalProductionTitle}
+                        </div>
+                        <hr className='horizontal-line'/>
+                        <div className='service-description'>
+                            {translate.agriculturalProductionDescription}
+                        </div>
+                </div>
+                <div className='service-block fade-right'>
+                        <FaTruckMoving className='service-icon'/>
+                        <div className='service-label'>
+                            {translate.freightTitle}
+                        </div>
+                        <hr className='horizontal-line'/>
+                        <div className='service-description'>
+                            {translate.freightDescription}
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
