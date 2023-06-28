02daa5b5e025884ce6c71c10cef6dfddd8ca7edd
Kolia Parasochka <koliaparasochka99@gmail.com>
2023-06-28 10:56:56 +0300

    updated About page

--- webclient/src/components/About.js
+++ webclient/src/components/About.js
@@ -4,23 +4,34 @@ function About(props) {
     const translate = props.content;
 
     return (
-        <div id="about" className="about-section">
-            <FadeInSection>
-                <div className="about-title">{translate.aboutTitle}</div>
-                <p className="about-description fade-text">
-                    {translate.about1}      
-                </p>
-                <p className="about-description fade-text">
-                    {translate.about2}
-                </p>
-                <p className="about-description fade-text">
-                    {translate.about3}
-                </p>
-                <p className="about-description fade-text">
-                    {translate.about4}
-                </p>
-            </FadeInSection>
+        <div>
+            <div id="about" className="about-section">
+                <FadeInSection>
+                    <div className="about-title">{translate.aboutTitle}</div>
+                    <p className="about-description fade-text">
+                        {translate.about1}      
+                    </p>
+                    <p className="about-description fade-text">
+                        {translate.about2}
+                    </p>
+                    <p className="about-description fade-text">
+                        {translate.about3}
+                    </p>
+                    <p className="about-description fade-text">
+                        {translate.about4}
+                    </p>
+                </FadeInSection>
+            </div>
+            <div id="about" className="about-section-ceo">
+                <FadeInSection direction="fade-bottom">
+                    <p className="about-description fade-text">
+                        {translate.aboutCEO}      
+                    </p>
+                </FadeInSection>
+            </div>
         </div>
+
+        
     )
 }
 
