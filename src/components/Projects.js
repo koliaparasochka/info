274ee5464f01187257898aac76e456f322d173c3
Kolia Parasochka <koliaparasochka99@gmail.com>
2024-08-22 14:12:24 +0300

    x

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,10 @@ export default class Projects extends React.Component {
         this.checkTranslate = this.checkTranslate.bind(this);
     }
 
+    componentDidMount() {
+        this.lazyLoading();
+    }
+
     componentDidUpdate() {
         this.checkTranslate();
     }
@@ -32,6 +36,35 @@ export default class Projects extends React.Component {
         }
     }
 
+    lazyLoading() {
+        document.addEventListener("DOMContentLoaded", function() {
+            var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+          
+            if ("IntersectionObserver" in window) {
+              var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+                entries.forEach(function(video) {
+                  if (video.isIntersecting) {
+                    for (var source in video.target.children) {
+                      var videoSource = video.target.children[source];
+                      if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                        videoSource.src = videoSource.dataset.src;
+                      }
+                    }
+          
+                    video.target.load();
+                    video.target.classList.remove("lazy");
+                    lazyVideoObserver.unobserve(video.target);
+                  }
+                });
+              });
+          
+              lazyVideos.forEach(function(lazyVideo) {
+                lazyVideoObserver.observe(lazyVideo);
+              });
+            }
+          });
+    }
+
     render() {
         return (
             <div>
@@ -46,14 +79,14 @@ export default class Projects extends React.Component {
                                 <div className='project-video'>
                                     { 
                                         this.state.language === 'en' && 
-                                        <video preload="none" className="video-file" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-public-en.PNG'>
+                                        <video className="lazy video-file" playsinline preload="none" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-public-en.PNG'>
                                             <source src='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-public-en.mp4' type="video/mp4" /> 
                                         </video>
                                     }
 
                                     { 
                                         this.state.language === 'ua' && 
-                                        <video preload="none" className="video-file" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-public-ua.PNG'>
+                                        <video className="lazy video-file" playsinline preload="none" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-public-ua.PNG'>
                                             <source src='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-ua.mp4' type="video/mp4" /> 
                                         </video>
                                     }
@@ -73,14 +106,14 @@ export default class Projects extends React.Component {
                                 <div className='project-video'>
                                     { 
                                         this.state.language === 'en' && 
-                                        <video preload="none" className="video-file" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-en.PNG'>
+                                        <video className="lazy video-file" playsinline preload="none" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-en.PNG'>
                                             <source src='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-en.mp4' type="video/mp4" /> 
                                         </video>
                                     }
 
                                     { 
                                         this.state.language === 'ua' && 
-                                        <video preload="none" className="video-file" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-ua.PNG'>
+                                        <video className="lazy video-file" playsinline preload="none" controls poster='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-ua.PNG'>
                                             <source src='https://nicksoft2020info.blob.core.windows.net/nicksoft2020info/Eden2012-management-ua.mp4' type="video/mp4" /> 
                                         </video>
                                     }
